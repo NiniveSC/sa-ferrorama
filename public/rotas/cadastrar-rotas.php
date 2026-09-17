2eec0375cf0fbd0fcd473215e57cc28cecafa784
Nínive <ninive_cararo@estudante.sesisenai.org.br>
2026-09-17 12:10:00 -0300

    add - comecei a fazer a tela e o css de cadastro de rotas

---
+++ public/rotas/cadastrar-rotas.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastrar Rotas</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <link rel="stylesheet" href="../../assets/style/style.css">
+</head>
+
+<body class="pagina_cadastro_user">
+    <nav class="navbar navbar-expand-lg navbar-dark">
+        <div class="container-fluid">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-between" id="navbar-Nav">
+                <ul class="navbar-nav me-auto">
+                    <li class="nav-item mx-3">
+                        <a class="nav-link" href="tela-geral-home.php">Dashboard</a>
+                    </li>
+                    <li class="nav-item mx-3">
+                        <a class="nav-link" href="tela-lista-sensores.php">Sensores</a>
+                    </li>
+                    <li class="nav-item mx-3">
+                        <a class="nav-link" href="tela-lista-trens.php">Trens</a>
+                    </li>
+                    <li class="nav-item mx-3">
+                        <a class="nav-link" href="tela-lista-rotas.php">Rotas</a>
+                    </li>
+                    <li class="nav-item mx-3">
+                        <a class="nav-link" href="funcionarios-cadastrados.php">Funcionário</a>
+                    </li>
+                    <li class="nav-item mx-3">
+                        <a class="nav-link" href="tela-relatorios.php">Relatórios</a>
+                    </li>
+
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                            aria-expanded="false">
+                            NomeAdmin
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="tela-login.php">Sair</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+<div class="pg-cadastro-rotas">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-12 col-md-10 col-lg-7">
+
+                    <div class="card card-cadastro-rotas p-4 text-center border-0">
+                        <div class="card-body p-4">
+
+                            <h2 class="card-title fw-bold mb-4">Cadastrar Rotas</h2>
+
+                            <div class="box-campos-rotas p-4 mx-auto">
+                                <form id="form-cadastro-rotas">
+    
+    <footer>
+
+    </footer>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI"
+        crossorigin="anonymous"></script>
+    <script src="../script/tela-cadastro-rotas.js"></script>
+</body>
+
+</html>
